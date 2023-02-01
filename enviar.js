d523b17fb45c33f953197387bec816b4ecc5e5f6
Natanssilva <natanssilva10@gmail.com>
2023-02-01 12:12:49 -0300

    readme

--- enviar.js
+++
@@ -1,7 +0,0 @@
-
-let send = ()=>{
-    firstName.innerHTML = `<p>${firstName} </p>`;
-    surname.innerHTML += `<p>${surname} </p>`;
-    weigh.innerHTML += `<p>${weigh} </p>`;
-    heigh.innerHTML += `<p>${heigh} </p>`;
-}
